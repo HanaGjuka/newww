cdaefe25059a180c677392440a26eeed5c153966
blerta <bpemaj18@gmail.com>
2023-06-02 21:24:40 +0200

    us20 completed

---
+++ src/test/java/group2/us20.java
@@ -0,0 +1,8 @@
+package test.java.group2;
+
+public class us20 {
+    public static void main(String[] args) {
+        System.out.println("us 20 completed");
+        System.out.println("us 30 completed");
+    }
+}
